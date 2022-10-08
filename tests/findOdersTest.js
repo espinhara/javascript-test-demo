0ac4d3e54f455b384fd535e85d977c6d4c200779
Gabriel Espinhara Da Silva <gabrielespinhara13@gmail.com>
2022-10-07 22:08:38 -0300

    clean code

--- tests/findOdersTest.js
+++ tests/findOdersTest.js
@@ -5,10 +5,10 @@ describe('find oders', ()=>{
         const arr = [1.7889, 30.51]
         findOders.sumOders(arr).should.be.equal(32.2989)
     })
-    it('find item in oders list return Item not found', () =>{
+    it('find item in oder list return Item not found', () =>{
         findOders.findItemOderList("broom").should.be.equal('Item not found')
     })
-    it('find item and return item', ()=>{
+    it('find item in oder list and return item', ()=>{
         findOders.findItemOderList("Chicken").should.be.equal('{"name":"Chicken pizza","value":0}')
     })
 })
