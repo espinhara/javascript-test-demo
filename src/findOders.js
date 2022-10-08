783820e3cb966f14625039228558b63ee3c2824c
Gabriel Espinhara Da Silva <gabrielespinhara13@gmail.com>
2022-10-07 22:04:22 -0300

    any tests

--- src/findOders.js
+++ src/findOders.js
@@ -1,3 +1,14 @@
+const oderList = [
+    {name :'Chicken pizza', value: 0},
+    {name :'Pepperoni pizza with catupiry', value: 0},
+    {name :'Pepperoni pizza', value: 0},
+    {name :'chocolate pizza', value: 0},
+    {name :'Portuguese pizza', value: 0},
+    {name :'Brazilian pizza', value: 0},
+    {name :'Italian pizza', value: 0},
+    {name :'Broccoli pizza with bacon', value: 0},
+
+]
 
 const sumOders = (arr)=>{
     let value= 0
@@ -6,8 +17,16 @@ const sumOders = (arr)=>{
     })
     return value;
 }
+const findItemOderList = (item)=>{
+    let has = oderList.filter(f=> f.name.replace(' pizza', '') == item)
+    let message = ''
+    has.length != 0 && (message =  JSON.stringify(has[0]));
+    has.length == 0 && (message = 'Item not found')
 
+    return message
+}
 
 module.exports = {
-    sumOders
+    sumOders,
+    findItemOderList
 }
